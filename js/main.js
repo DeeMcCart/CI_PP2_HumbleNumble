c63470b84308d7e946af2874cf8b694ad512deba
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-05-08 22:04:30 +0000

    Buttons: Add functionality to capture selected letters

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+console.log('Hello World!'); // Adding a console output for testing purposes //
+document.addEventListener("DOMContentLoaded",() => { 
+const keys=document.querySelectorAll('.selection-row button');
+console.log (keys);
 
+for (let i=0; i<keys.length; i++) {
+    keys[i].onclick =({target}) => {
+        const key=target.getAttribute("data-key");
+        console.log(key); // Adding a console output for testing purposes //
+    };
+}
+}
+);

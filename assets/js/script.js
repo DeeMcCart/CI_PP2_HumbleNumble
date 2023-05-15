c4f9f1ea964c16f99a22e8565d4da5400e84aa57
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-05-15 21:50:43 +0000

    Add logic to capture user selected keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
-console.log('Hello World!'); // Adding a console output for testing purposes //
 document.addEventListener("DOMContentLoaded", () => {
+    console.log('Dom Content loaded!'); // Adding a console output for testing purposes //
 
-    // Checking that each of the keyboard entries is setup and associated with a number or character .....    
-    const keys = document.querySelectorAll('.selection-row button');
-    console.log(keys);
 
+    // the below logic umoved to function captureAttempt()
     // Using logic identified by Ian Lenehan 'Build a Wordle clone using HTML, CSS & Javascript' to confirm that each key is populated //
-    for (let i = 0; i < keys.length; i++) {
-        keys[i].onclick = ({
-            target
-        }) => {
-            const key = target.getAttribute("data-key");
-            console.log(key); // Adding a console output for testing purposes, to show which key has been clicked //
-        };
-    }
+    //for (let i = 0; i < keys.length; i++) {
+      //  keys[i].onclick = ({
+        //    target
+        //}) => {
+         //   const key = target.getAttribute("data-key");
+           // console.log(key); // Adding a console output for testing purposes, to show which key has been clicked //
+        //};
+   // }
+      captureAttempt();
 });
 
 // Define an array to hold solution values.  This is a multi-dimension array which can be added to as new solutions are generated daily 
 // For now it will be used to pick a random entry from the array for that day's guess 
 function initSolution() {
     let possSolution=[
-    [1, "+", 2, "*", 5, 11, "Unused"]
+    [1, "+", 2, "*", 5, 11, "Unused"],
     [10, '/', 5, "*", 20, 40, "Unused"],
     [20, '*', 10, '/', 4, 50, "Unused"],
     ];
@@ -140,4 +139,32 @@ function calcArray(arrayParam) {
 
     }
     return (result);
+}
+
+function captureAttempt() {
+    // Checking that each of the keyboard entries is setup and associated with a number or character .....    
+    const keys = document.querySelectorAll('.selection-row button');
+    let userAttempt=[0, "+", 0, "+", 0, 0];
+    for (j=0; j<userAttempt.length; j++) {
+        console.log('value of j when entering loop is' +j);
+    // Using logic identified by Ian Lenehan 'Build a Wordle clone using HTML, CSS & Javascript' to confirm that each key is populated //
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({
+            target
+        }) => {
+            userAttempt[j] = target.getAttribute("data-key");
+            console.log('char '+j +'is: '+ userAttempt[j]); // Adding a console output for testing purposes, to show which key has been clicked //
+        j++;
+        }
+    }
+}
+
+    console.log('Attempt is: ', userAttempt);
+    
+}
+
+function checkSolution(arrayParam) {
+console.log('Array passed is ' + arrayParam[0] + ' ' + arrayParam[1] + ' ' + arrayParam[2] + ' ' + arrayParam[3] + ' ' + arrayParam[4] );
+    console.log('ArrayParam[1]is' + arrayParam[1]);
+    
 }

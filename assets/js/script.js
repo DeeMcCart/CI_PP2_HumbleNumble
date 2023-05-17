da2477b60e0317e9eaf2fd6c999e5f94507a3bc0
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-05-17 19:29:54 +0000

    Modify function to capture user guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,11 +54,11 @@ function initSolution() {
 
 // DMcC 17/05/23 The function below writeLetter is used to write a single letter of a guess onto the screen //
 function writeLetter(rowNum, position, guessItem) {
-   console.log("In function writeLetter!");
-   console.log("writing to position: row: "+('row' + rowNum)+" position: "+position + " Guessed item: "+guessItem);
-        let fred= document.getElementsByClassName('row' + rowNum)[position];
-        console.log("Character found at position is: " +fred);
-        fred.innerHTML = guessItem;
+    console.log("In function writeLetter!");
+    console.log("writing to position: row: " + ('row' + rowNum) + " position: " + position + " Guessed item: " + guessItem);
+    let fred = document.getElementsByClassName('row' + rowNum)[position];
+    console.log("Character found at position is: " + fred);
+    fred.innerHTML = guessItem;
 }
 
 
@@ -70,11 +70,11 @@ function writeLetter(rowNum, position, guessItem) {
 function writeGuess(rowNum, guessArray) {
     console.log("In function writeGuess!");
     console.log("guessArray parameter is: " + guessArray + " type: " + typeof (guessArray));
-   // Below not required as code above lists all elements of the array 
-   // console.log("guessArray parameter is: ")
-   // for (i = 0; i < guessArray.length; i++) {
-   //     console.log(guessArray[i]);
-   // }
+    // Below not required as code above lists all elements of the array 
+    // console.log("guessArray parameter is: ")
+    // for (i = 0; i < guessArray.length; i++) {
+    //     console.log(guessArray[i]);
+    // }
     // DMcC 17/05/23 can strip out the 2 lines below and replace with calculation within array reference itelf // 
     let guessRow = document.getElementsByClassName('row' + rowNum);
     console.log("guessRow array before population: " + guessRow);
@@ -191,28 +191,57 @@ function calcArray(arrayParam) {
 }
 
 function captureAttempt() {
+    // DMcC 17/05/23 Start with capturing what attempt number we are on: //
+    let attemptNum = document.getElementById('attemptNum').innerHTML;
+    console.log("Within captureAttempt function, attempt number is " + attemptNum);
+    let userTry = [0, "+", 0, "+", 0, 0];
+    userTry[0] = keyClick();
+    console.log('first key pressed is ' + userTry[0]); // Adding a console output for testing purposes, to show which key has been clicked //
+    writeLetter(attemptNum, 0, userTry[0]);
+    userTry[1] = keyClick();
+    writeLetter(attemptNum, 1, userTry[1]);
+    
     // DMcC 17/05/23 this function needs a little more work as the onclick function needs to be adapted to populate an array //
     // Checking that each of the keyboard entries is setup and associated with a number or character .....    
-    const keys = document.querySelectorAll('.selection-row button');
-console.log(keys);
-    let userAttempt = [0, "+", 0, "+", 0, 0];
-    for (j = 0; j < 5; j++) {
-        console.log('value of j when entering loop is' + j);
-        // Using logic identified by Ian Lenehan 'Build a Wordle clone using HTML, CSS & Javascript' to confirm that each key is populated //
-        // DMcC 17/05/23 This is weird because its like a loop that never exits; I need a loop that executes fro the right number of guess times
-        for (let i = 0; i < keys.length; i++) {
-            keys[i].onclick = ({target}) => {
-                userAttempt[j] = target.getAttribute("data-key");
-                console.log('char ' +j + 'is: ' +userAttempt[j]); // Adding a console output for testing purposes, to show which key has been clicked //
-                writeLetter(1, 1, userAttempt[j]);
-             }
-        }
-    }
-
-    console.log('Attempt is: ', userAttempt);
+    //    
+    //  console.log(keys);
+    //let userAttempt = [0, "+", 0, "+", 0, 0];
+    //for (j = 0; j < 5; j++) {
+    //   console.log('value of j when entering loop is' + j);
+    // Using logic identified by Ian Lenehan 'Build a Wordle clone using HTML, CSS & Javascript' to confirm that each key is populated //
+    // DMcC 17/05/23 This is weird because its like a loop that never exits; I need a loop that executes fro the right number of guess times
 
 }
 
+// DMcC 17/05/23 the below commented out as it was an approach to capturing keyclicks that was designed by someone else //
+// and just confused me.  Replaced with a simpler keyclick function //
+// function keyClick() {
+//    console.log('Within function keyClick');
+//    const keys = document.querySelectorAll('.selection-row button');
+//    for (let i = 0; i < keys.length; i++) {
+//        keys[i].onclick = ({
+//            target
+//        }) => {
+//            const key = target.getAttribute("data-key");
+//            console.log("function keyClick says " + key);
+//            console.log("this is where we need to populate an array of j characters");
+//            return key; // DMcC 17/05/23 this causes the function to exit but then it keeps running - why?  should this be the outer function?
+//        };
+//    }
+//}
+
+// The below function keyClick is invoked when the user clicks any of the buttons //
+// This function needs to establish: //
+// - which attempt is this?  
+function keyClick(selectedKey) {
+    // DMcC 17/05/23 Start with capturing what attempt number we are on: //
+   let attemptNum = document.getElementById('attemptNum').innerHTML;
+   // DMcC 17/05/23 also determine what is the next column number to populate: //
+   let colNum = document.getElementById('colNum').innerHTML;
+   console.log("Within keyClick function, attempt number " + attemptNum + "column Number "+colNum + "character "+ selectedKey);
+   writeGuess(attemptNum, colNum,selectedKey);
+}
+    
 function checkSolution(arrayParam) {
     console.log('Array passed is ' + arrayParam[0] + ' ' + arrayParam[1] + ' ' + arrayParam[2] + ' ' + arrayParam[3] + ' ' + arrayParam[4]);
     console.log('ArrayParam[1]is' + arrayParam[1]);
